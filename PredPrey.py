01a5b2a0cca29869c22b4ff1ffc2745178f97671
Andrew MostlySparks <82421685+AndrewStruthers-MostlySparks@users.noreply.github.com>
2021-11-10 09:54:19 -0800

    Added labels to the graphs

--- PredPrey.py
+++ PredPrey.py
@@ -6,7 +6,8 @@ import matplotlib.pyplot as plt
 V0 = float(input("Enter the initial population of predators: "))
 U0 = float(input("Enter the initial population of prey: "))
 alpha = 0.1
-t = np.linspace(0,100)
+t = np.linspace(0,100, num=1000)
+#print(t)
 
 #Defining predator-prey as a function of time
 def predPrey(y,t,alpha):
@@ -18,5 +19,10 @@ def predPrey(y,t,alpha):
 #Specify y0 and then solve the system of differential equations, then plot   
 y0 = U0,V0
 y = odeint(predPrey, y0, t, args=(alpha,))
+#print(y)
 plt.plot(t,y)
+plt.legend(["Prey", "Predator"])
+plt.ylabel("Population")
+plt.xlabel("Time")
+plt.title("Predator VS. Prey Model")
 plt.show()
